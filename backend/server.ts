6793c5e07672708db1c6303fa536f68c7e379b6b
Phuc Le <phuclevinh2000@gmail.com>
2021-11-22 20:17:03 +0200

    adding dotenv

--- backend/server.ts
+++ backend/server.ts
@@ -1,27 +1,31 @@
-import express, {Application, Request, Response} from "express"
-import products from "./data/products"
-import cargotery from "./data/cargotery"
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import products from './data/products';
+import cargotery from './data/cargotery';
 
-const app: Application = express()
+dotenv.config();
+
+const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send("API is running")
-})
+  res.send('API is running');
+});
 
-app.get('/api/products',(req: Request, res: Response) => {
-  res.json(products)
-})
+app.get('/api/products', (req: Request, res: Response) => {
+  res.json(products);
+});
 
-app.get('/api/cargotery',(req: Request, res: Response) => {
-  res.json(cargotery)
-})
+app.get('/api/cargotery', (req: Request, res: Response) => {
+  res.json(cargotery);
+});
 
-app.get('/api/products/:id',(req: Request, res: Response) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
-app.get('/api/')
+app.get('/api/products/:id', (req: Request, res: Response) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(5000, () => {
-  console.log("Started")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
